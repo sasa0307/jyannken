bb25ce56e5b1841f8c9ea957f562f51532dac103
hanata <s.hanata.sys23@morijyobi.ac.jp>
2024-07-19 13:54:17 +0900

    close#1 close#2

---
+++ src/Choki.java
@@ -0,0 +1,22 @@
+package src;
+public class Choki implements Hand{
+    private String HAND_NAME = "チョキ";
+    /**
+     手の強弱を比較する。
+     返り値 -1 : 自身が引数で指定した手より弱い
+     　　　　0 : 自身が引数で指定した手と同じ
+     　　　　1 : 自身が引数で指定した手より強い
+     */
+    public int compare(Hand hand) {
+        if(hand instanceof Choki) {    //チョキ　vs choki（自分）
+            return 0;
+        } else if(hand instanceof Pa) {    //パー　vs choki（自分）
+            return 1;
+        } else {
+            return -1;    //グー　vs choki（自分）
+        }
+    }
+    public String getHandName() {
+        return this.HAND_NAME;
+    }
+}
